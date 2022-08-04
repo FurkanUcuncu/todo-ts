d169b0852bd469b75733b1ac49bf7a5f60f35c18
FurkanUcuncu <73463054+FurkanUcuncu@users.noreply.github.com>
2022-08-04 23:07:35 +0300

    Change component name, fixed key bug

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from "react-native";
+import {View} from "react-native";
 import {useAppDispatch, useAppSelector} from '../../hooks/redux-hooks';
 import { TodoType } from '../../models/TodoTypes';
 import Todo from './Todo';
@@ -9,7 +9,7 @@ interface IProps {
     handleEditTodo:(todo:TodoType)=>void
 }
 
-const TodoSwipeList: React.FC<IProps> = props => {
+const TodoList: React.FC<IProps> = props => {
     const dispatch = useAppDispatch()
     const { todos } = useAppSelector(state => state?.todo)
 
@@ -24,9 +24,8 @@ const TodoSwipeList: React.FC<IProps> = props => {
             {
                 todos.map((item:TodoType) => {
                     return (
-                        <View style={{marginBottom:15}}>
+                        <View key={item.id} style={{marginBottom:15}}>
                             <Todo
-                                key={item.id}
                                 handleEditTodo={()=>props.handleEditTodo(item)}
                                 onSwipe={()=>deleteRow(item)}
                                 {...{ item }}
@@ -39,79 +38,4 @@ const TodoSwipeList: React.FC<IProps> = props => {
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#f4f4f4',
-        flex: 1,
-    },
-    backTextWhite: {
-        color: '#FFF',
-    },
-    todoContainer: {
-        justifyContent:'center',
-        backgroundColor: '#FFF',
-        borderRadius: 5,
-        height: 60,
-        margin: 5,
-        marginBottom: 15,
-        shadowColor: '#999',
-        shadowOffset: {width: 0, height: 1},
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 5,
-    },
-    rowBack: {
-        alignItems: 'center',
-        backgroundColor: '#DDD',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 15,
-        margin: 5,
-        marginBottom: 15,
-        borderRadius: 5,
-    },
-    rowFrontVisible: {
-        backgroundColor: '#FFF',
-        borderRadius: 5,
-        height: 60,
-        padding: 10,
-        marginBottom: 15,
-    },
-    backRightBtn: {
-        alignItems: 'flex-end',
-        bottom: 0,
-        justifyContent: 'center',
-        position: 'absolute',
-        top: 0,
-        width: 75,
-        paddingRight: 17,
-    },
-    backRightBtnLeft: {
-        backgroundColor: '#1f65ff',
-        right: 75,
-    },
-    backRightBtnRight: {
-        backgroundColor: 'red',
-        right: 0,
-        borderTopRightRadius: 5,
-        borderBottomRightRadius: 5,
-    },
-    trash: {
-        height: 25,
-        width: 25,
-        marginRight: 7,
-    },
-    title: {
-        fontSize: 14,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        color: '#666',
-    },
-    details: {
-        fontSize: 12,
-        color: '#999',
-    },
-})
-
-export default TodoSwipeList;
+export default TodoList;
