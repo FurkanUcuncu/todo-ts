2f54d46e33b3217110693e3250e3daeba0960d53
FurkanUcuncu <73463054+FurkanUcuncu@users.noreply.github.com>
2022-08-04 23:32:07 +0300

    Add send button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
-import {KeyboardAvoidingView, View} from "react-native";
-import {TextInput} from 'react-native-paper';
+import {KeyboardAvoidingView, View, StyleSheet, Text} from "react-native";
+import {TextInput,IconButton, useTheme} from 'react-native-paper';
 import {todoActions} from '../store/todos/todoSlice';
 import {useAppDispatch, useAppSelector} from "../hooks/redux-hooks";
 import Layout from "../components/layout/Layout";
 import {TodoType} from "../models/TodoTypes";
 import TodoList from '../components/todo/TodoList';
-import { Text } from 'react-native';
 
 const Home: React.FC = () => {
     const dispatch = useAppDispatch()
+    const {colors} = useTheme()
     const { todos } = useAppSelector(state => state?.todo)
     const [text,setText] = useState<string>("")
     const handleEditTodo = (currentTodo:TodoType) => {
         dispatch(todoActions.updateTodo(todos.map((todo:TodoType)=>todo.id === currentTodo.id ? {...todo,completed:!currentTodo.completed} : todo)))
     }
     const handleAddTodo = () => {
-        dispatch(todoActions.setTodos({id:Math.random(),text,completed:false}))
+        if (text !== "") {
+            dispatch(todoActions.setTodos({id:Math.random(),text,completed:false}))
+        }
         setText('')
     }
     return (
@@ -29,18 +31,40 @@ const Home: React.FC = () => {
                             handleEditTodo={handleEditTodo}
                         />
                 }
-                <TextInput
-                    autoCorrect={false}
-                    style={{backgroundColor:'#fff'}}
-                    mode="outlined"
-                    label="Type a to do"
-                    value={text}
-                    onChangeText={text => setText(text)}
-                    onSubmitEditing={handleAddTodo}
-                />
+                <View style={styles.bottomContainer}>
+                    <TextInput
+                        autoCorrect={false}
+                        style={{backgroundColor:'#fff',flex:1, borderRadius:20}}
+                        mode="outlined"
+                        label="Type a to do"
+                        value={text}
+                        onChangeText={text => setText(text)}
+                        onSubmitEditing={handleAddTodo}
+                    />
+                    <IconButton
+                        style={[styles.sendBtn, { borderColor: colors.primary }]}
+                        icon="send"
+                        color={colors.primary}
+                        size={20}
+                        onPress={handleAddTodo} 
+                    />
+                </View>
             </KeyboardAvoidingView>
         </Layout>
     );
 }
+
+const styles = StyleSheet.create({
+    bottomContainer: {
+        flexDirection: 'row',
+        alignItems:'center'
+    },
+    sendBtn: {
+        borderWidth: 1,
+        height: 55,
+        width: 55,
+        borderRadius:22.5
+    }
+})
 
 export default Home;
